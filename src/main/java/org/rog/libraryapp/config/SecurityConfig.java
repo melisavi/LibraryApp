96ef60c3bbe469815dedc19a5cc00dd4503587ed
Elena Rog <rog.elena.yu@gmail.com>
2025-03-09 13:38:59 +0300

    security: form login

--- src/main/java/org/rog/libraryapp/config/SecurityConfig.java
+++ src/main/java/org/rog/libraryapp/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                //WARN: We use sessions, and csrf must be enabled but we disabled it for simplification.
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/v1/registration").permitAll()
