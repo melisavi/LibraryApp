b96643f6be46af5a324964403ced757c37836a35
Elena Rog <rog.elena.yu@gmail.com>
2025-03-13 19:10:22 +0300

    security: basic auth

--- src/main/java/org/rog/libraryapp/config/SecurityConfig.java
+++ src/main/java/org/rog/libraryapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,12 +17,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                //WARN: We use sessions, and csrf must be enabled but we disabled it for simplification.
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/v1/registration").permitAll()
                         .anyRequest().authenticated())
-                .formLogin();
+                .httpBasic();//.formLogin();
         return http.build();
     }
 
