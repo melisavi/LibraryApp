dcb1fd7f8a0e84bc69a60ecf47ba84a1adbb38cf
Elena Rog <rog.elena.yu@gmail.com>
2025-03-15 11:58:20 +0300

    Project refactoring: library-auth service template

---
+++ library-auth/src/main/java/org/rog/library/auth/LibraryAuthApp.java
@@ -0,0 +1,11 @@
+package org.rog.library.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAuthApp.class, args);
+    }
+}
--- library-core/src/main/java/org/rog/library/core/LibraryCoreApp.java
+++ library-core/src/main/java/org/rog/library/core/LibraryCoreApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryApp {
+public class LibraryCoreApp {
     public static void main(String[] args){
-        SpringApplication.run(LibraryApp.class);
+        SpringApplication.run(LibraryCoreApp.class);
     }
 }
